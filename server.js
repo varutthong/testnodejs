28f652bbafe3f688c489058640aa0d81829065c7
varutthong <varutthong123@gmail.com>
2022-12-08 22:13:56 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const mysql = require('mysql');
+
+const app = express();
+app.use(express.json());
+
+// Mysql connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'leaderboard',
+    port: '3306'
+ 
+})
+
+connection.connect((err) => {
+    if(err) {
+        console.log('Error connecting to MySQL database = ', err)
+        return;
+    }
+    console.log('Mysql successfully connected!');
+})
+
+// create routes
+app.post("/create", async (req, res) => {
+    const { name, score} =req.body;
+    try{
+        connection.query(
+            "INSERT INTO user(name, score) VALUES(?, ?)",
+            [name, score],
+            (err, results, fields) => {
+                if(err){
+                    console.log("Error while inserting a user into the database", err);
+                    return res.status(400).send();
+                }
+                return res.status(201).json({ message: "New user successfully created!"});
+            }
+        )
+    } catch(err){
+        console.log(err);
+        return res.status(500).send();
+    }
+})
+
+// READ
+app.get("/read", async (req, res) => {
+    try{
+        connection.query("SELECT * FROM user", (err, results, fields) => {
+            if(err){
+                console.log(err);
+                return res.status(400).send();
+            }
+            res.status(200).json(results)
+        })
+
+    }catch(err){
+        console.log(err);
+        return res.status(500).send();
+
+    }
+})
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
