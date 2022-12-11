010d2d3e76047d7688e60e03d74b167ed4250cb8
varutthong <varutthong123@gmail.com>
2022-12-11 18:23:04 +0700

    20 commit

--- index.js
+++ index.js
@@ -10,65 +10,65 @@ app.use((req, res, next)  => {
     })
 
 // Mysql connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'leaderboard',
-    port: '3306'
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'leaderboard',
+//     port: '3306'
  
-})
+// })
 
-connection.connect((err) => {
-    if(err) {
-        console.log('Error connecting to MySQL database = ', err)
-        return;
-    }
-    console.log('Mysql successfully connected!');
-})
+// connection.connect((err) => {
+//     if(err) {
+//         console.log('Error connecting to MySQL database = ', err)
+//         return;
+//     }
+//     console.log('Mysql successfully connected!');
+// })
 
-// create routes
-app.post("/create", async (req, res) => {
-    const { name, score} =req.body;
-    try{
-        connection.query(
-            "INSERT INTO user(name, score) VALUES(?, ?)",
-            [name, score],
-            (err, results, fields) => {
-                if(err){
-                    console.log("Error while inserting a user into the database", err);
-                    return res.status(400).send();
-                }
-                return res.status(201).json({ message: "New user successfully created!"});
-            }
-        )
-    } catch(err){
-        console.log(err);
-        return res.status(500).send();
-    }
-})
+// // create routes
+// app.post("/create", async (req, res) => {
+//     const { name, score} =req.body;
+//     try{
+//         connection.query(
+//             "INSERT INTO user(name, score) VALUES(?, ?)",
+//             [name, score],
+//             (err, results, fields) => {
+//                 if(err){
+//                     console.log("Error while inserting a user into the database", err);
+//                     return res.status(400).send();
+//                 }
+//                 return res.status(201).json({ message: "New user successfully created!"});
+//             }
+//         )
+//     } catch(err){
+//         console.log(err);
+//         return res.status(500).send();
+//     }
+// })
 
-// READ
-app.get("/read", async (req, res) => {
-    const results = [{"name":"rut","score":"10"},{"name":"non","score":"20"},{"name":"max","score":"30"},{"name":"man","score":"40"},{"name":"gae","score":"50"}]
-    try{
-        // connection.query("SELECT * FROM user", (err, results, fields) => {
-        //     if(err){
-        //         console.log(err);
-        //         return res.status(400).send();
-        //     }
-        //     res.status(200).json(results)
-        // })
-        res.status(200).json(results)
-    }catch(err){
-        console.log(err);
-        return res.status(500).send();
+// // READ
+// app.get("/read", async (req, res) => {
+//     const results = [{"name":"rut","score":"10"},{"name":"non","score":"20"},{"name":"max","score":"30"},{"name":"man","score":"40"},{"name":"gae","score":"50"}]
+//     try{
+//         // connection.query("SELECT * FROM user", (err, results, fields) => {
+//         //     if(err){
+//         //         console.log(err);
+//         //         return res.status(400).send();
+//         //     }
+//         //     res.status(200).json(results)
+//         // })
+//         res.status(200).json(results)
+//     }catch(err){
+//         console.log(err);
+//         return res.status(500).send();
 
-    }
-})
+//     }
+// })
 
 app.get('/', (req, res) => {
-    return res.send('111111111111111111111111')
+    return res.send('Hello world')
     
 })
 
