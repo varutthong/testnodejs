912e7bed1262e0d81392703e619921ebc4b3c473
varutthong <varutthong123@gmail.com>
2022-12-15 18:06:12 +0700

    2

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser')
 const app = express();
 app.use(express.json());
 app.use((req, res, next)  => {
@@ -9,39 +7,10 @@ app.use((req, res, next)  => {
     next();
     })
 
-//Mysql connection
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'leaderboard',
-//     port: '3306'
- 
-// })
-
-// connection.connect((err) => {
-//     if(err) {
-//         console.log('Error connecting to MySQL database = ', err)
-//         return;
-//     }
-//     console.log('Mysql successfully connected!');
-// })
-
 // create routes
 // app.post("/create", (req, res) => {
-//     const { name, score} =req.body;
 //     try{
-//         connection.query(
-//             "INSERT INTO user(name, score) VALUES(?, ?)",
-//             [name, score],
-//             (err, results, fields) => {
-//                 if(err){
-//                     console.log("Error while inserting a user into the database", err);
-//                     return res.status(400).send();
-//                 }
-//                 return res.status(201).json({ message: "New user successfully created!"});
-//             }
-//         )
+//         
 //     } catch(err){
 //         console.log(err);
 //         return res.status(500).send();
@@ -49,22 +18,17 @@ app.use((req, res, next)  => {
 // })
 
 // READ
-app.get("/read", (req, res) => {
-    const results = 
+const results = 
         [      
             {name:"rut",score:10},
             {name:"max",score:20},
             {name:"non",score:30}                  
         ]
+const obj = Object.assign({}, results)
+
+app.get("/read", (req, res) => {
+    
     try{
-        
-        // connection.query("SELECT * FROM user", (err, results, fields) => {
-        //     if(err){
-        //         console.log(err);
-        //         return res.status(400).send();
-        //     }
-        //     res.status(200).json(results)
-        // })
          res.status(200).json({user:results});
     }catch(err){
         console.log(err);
